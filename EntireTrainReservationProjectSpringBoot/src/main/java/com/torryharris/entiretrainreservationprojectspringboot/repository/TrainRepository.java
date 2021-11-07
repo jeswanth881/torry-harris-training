4d337e99d56fb49945c115020bcdf4e44247fafd
jeswanth881 <70259677+jeswanth881@users.noreply.github.com>
2021-11-07 18:39:41 +0530

    Delete EntireTrainReservationProjectSpringBoot directory

--- EntireTrainReservationProjectSpringBoot/src/main/java/com/torryharris/entiretrainreservationprojectspringboot/repository/TrainRepository.java
+++
@@ -1,7 +0,0 @@
-package com.torryharris.entiretrainreservationprojectspringboot.repository;
-
-import com.torryharris.entiretrainreservationprojectspringboot.model.Train;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainRepository extends JpaRepository<Train,Integer> {
-}
